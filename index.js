4e5cc277bb9a8b208ba47483a7c2ba5f4663828b
Leah Hill <leah@verysandwich.com>
2018-07-11 19:18:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+  return (a + b)
+}
+function subtract(a, b) {
+  return (a - b)
+}
+function multiply(a, b) {
+  return (a * b)
+}
+function divide(a, b) {
+  return (a / b)
+}
+function inc(a) {
+  return (a + 1)
+}
+function dec(a) {
+  return (a - 1)
+}
+function makeInt(a) {
+  return parseInt(a)
+}
+function isNaN(adlkfaldkf) { 
+ return true 
+}
+function preserveDecimal(a) {
+return (a);
+}
+function parceDecimal(2.222) {
+return parseFloat(a);
+}
